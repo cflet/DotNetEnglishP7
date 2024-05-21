c0234f64864c5da42445d929c93e61f1b245caa7
Cheron Fletcher <127625161+cflet@users.noreply.github.com>
2024-05-20 21:45:33 -0500

    update BidList Domain

--- src/Domain/BidList.cs
+++ src/Domain/BidList.cs
@@ -8,26 +8,26 @@ namespace Dot.Net.WebApi.Domain
         [Key]
         public int BidListId { get; set; }
 
-        public string account { get; set; }
-        public string type { get; set; }
-        public double bidQuantity { get; set; }
-        public double askQuantity { get; set;}
-        public double bid { get; set; }
-        public double ask { get; set; }
-        public string benchmark { get; set; }
-        public TimestampAttribute  bidListDate { get; set; }
-        public string commentary { get; set; }
-        public string security { get; set; }
-        public string status { get; set; }
-        public string trader { get; set; }
-        public string book { get; set; }
-        public string creationName { get; set; }
-        public TimestampAttribute creationDate { get; set; }
-        public string revisionName { get; set; }
-        public TimestampAttribute revisionDate { get; set; }
-        public string dealName { get; set; }
-        public string dealType { get; set; }
-        public string souceListId { get; set; }
-        public string side { get; set; }
+        public string Account { get; set; }
+        public string Type { get; set; }
+        public double BidQuantity { get; set; }
+        public double AskQuantity { get; set;}
+        public double Bid { get; set; }
+        public double Ask { get; set; }
+        public string Benchmark { get; set; }
+        public TimestampAttribute  BidListDate { get; set; }
+        public string Commentary { get; set; }
+        public string Security { get; set; }
+        public string Status { get; set; }
+        public string Trader { get; set; }
+        public string Book { get; set; }
+        public string CreationName { get; set; }
+        public TimestampAttribute CreationDate { get; set; }
+        public string RevisionName { get; set; }
+        public TimestampAttribute RevisionDate { get; set; }
+        public string DealName { get; set; }
+        public string DealType { get; set; }
+        public string SouceListId { get; set; }
+        public string Side { get; set; }
     }
 }
