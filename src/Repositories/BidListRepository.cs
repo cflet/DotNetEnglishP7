2d216d81b899d96bc997b952cfdefeb81bc9a4e1
Cheron Fletcher <127625161+cflet@users.noreply.github.com>
2024-06-24 18:44:56 -0500

    add BidList repo

---
+++ src/Repositories/BidListRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dot.Net.WebApi.Data;
+using Dot.Net.WebApi.Domain;
+using System.Threading.Tasks;
+using WebApi.Repositories;
+
+namespace Dot.Net.WebApi.Repositories
+{
+    public class BidListRepository : IBidListRepository
+    {
+        public LocalDbContext DbContext { get; }
+
+        public BidListRepository(LocalDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        public BidList FindByBidListId(int bidlistid)
+        {
+            return DbContext.BidLists.FirstOrDefault(BidList => BidList.BidListId == bidlistid);
+        }
+
+        public BidList[] FindAll()
+        {
+            return DbContext.BidLists.ToArray();
+        }
+
+        public void Add(BidList bidlist)
+        {
+            DbContext.Add(bidlist);
+            DbContext.SaveChanges();
+        }
+
+        public void Update(BidList bidlist)
+        {
+            DbContext.Update(bidlist);
+            DbContext.SaveChanges();
+        }
+
+        public void Delete(BidList bidlist)
+        {
+            DbContext.Remove(bidlist);
+            DbContext.SaveChanges();
+        }
+    }
+}
+
---
+++ src/Repositories/IBidListRepository.cs
@@ -0,0 +1,17 @@
+﻿using Dot.Net.WebApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Repositories
+{
+    public interface IBidListRepository
+    {
+        BidList FindByBidListId(int bidlistid);
+        BidList[] FindAll();
+        void Add(BidList bidlist);
+        void Update(BidList bidlist);
+        void Delete(BidList bidlist);
+    }
+}
