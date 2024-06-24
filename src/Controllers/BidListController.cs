066ee372abaf1b310d180b35380cc1be713afcd0
Cheron Fletcher <127625161+cflet@users.noreply.github.com>
2024-06-24 18:45:46 -0500

    update BidList controller

--- src/Controllers/BidListController.cs
+++ src/Controllers/BidListController.cs
@@ -3,44 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Dot.Net.WebApi.Domain;
+using Dot.Net.WebApi.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
- 
+using WebApi.Repositories;
+
 namespace Dot.Net.WebApi.Controllers
 {
     [Route("[controller]")]
     public class BidListController : Controller
     {
-        [HttpGet("/")]
-        public IActionResult Home()
+        private IBidListRepository _bidlistRepository;
+        private ILogger<BidListController> _logger;
+
+        public BidListController(IBidListRepository bidListRepository, ILogger<BidListController> logger)
         {
-            return View("Home");
+            _bidlistRepository = bidListRepository;
+            _logger = logger;
+        }
+         
+        [HttpGet("/bidList/list")]
+        public IActionResult GetAll()
+        {
+            _logger.LogInformation("$$$*****Log Getting All BidList******$$$");
+            return Ok(_bidlistRepository.FindAll());
         }
 
-        [HttpGet("/bidList/validate")]
-        public IActionResult Validate([FromBody]BidList bidList)
+        [HttpGet("/bidList/list/{id}")]
+        public IActionResult GetABid(int id)
+        {
+            return Ok(_bidlistRepository.FindByBidListId(id));
+        }
+
+            [HttpPost("/bidList/add")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult AddBidList([FromBody] BidList bidList)
         {
             // TODO: check data valid and save to db, after saving return bid list
-            return View("bidList/add");
+            if (ModelState.IsValid)
+            {
+                _bidlistRepository.Add(bidList);
+                return Ok("Success");
+            } else
+            {
+                return BadRequest();
+                //add error log
+            } 
         }
 
-        [HttpGet("/bidList/update/{id}")]
-        public IActionResult ShowUpdateForm(int id)
-        {
-            return View("bidList/update");
-        }
 
-        [HttpPost("/bidList/update/{id}")]
-        public IActionResult UpdateBid(int id, [FromBody] BidList bidList)
+        [HttpPut("/bidList/update")]
+        public IActionResult UpdateBid([FromBody] BidList bidList)
         {
             // TODO: check required fields, if valid call service to update Bid and return list Bid
-            return Redirect("/bidList/list");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _bidlistRepository.Update(bidList);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                        return BadRequest("Invalid");
+                    //add error log
+                }
+            }
+            return Ok("Success");
         }
 
         [HttpDelete("/bidList/{id}")]
         public IActionResult DeleteBid(int id)
         {
-            return Redirect("/bidList/list");
+            //find bidList to delete
+            BidList bidlist = _bidlistRepository.FindByBidListId(id);
+
+            if (bidlist == null)
+            {
+                return BadRequest("Invalid");
+                //add error log
+            } else
+            {
+                _bidlistRepository.Delete(bidlist);
+                return Ok();
+            }
         }
     }
 }
