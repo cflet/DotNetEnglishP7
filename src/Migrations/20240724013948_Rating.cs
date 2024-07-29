035020ca44cbbd54fbca48a2ab6b8a3b691d660c
Cheron Fletcher <127625161+cflet@users.noreply.github.com>
2024-07-28 19:43:56 -0500

    Rating migration

---
+++ src/Migrations/20240724013948_Rating.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ratings",
+                columns: table => new
+                {
+                    RatingId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MoodysRating = table.Column<string>(nullable: true),
+                    SandRating = table.Column<string>(nullable: true),
+                    FitchRating = table.Column<string>(nullable: true),
+                    OrderNumber = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ratings", x => x.RatingId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ratings");
+        }
+    }
+}
