9a14afb8f6b9a0553804a7cabc19a7840b07d0a2
Cheron Fletcher <127625161+cflet@users.noreply.github.com>
2024-07-29 22:09:46 -0500

    RuleName Migration

---
+++ src/Migrations/20240729010508_RuleName.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class RuleName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RuleNames",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Json = table.Column<string>(nullable: true),
+                    Template = table.Column<string>(nullable: true),
+                    SqlStr = table.Column<string>(nullable: true),
+                    SqlPart = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RuleNames", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RuleNames");
+        }
+    }
+}
